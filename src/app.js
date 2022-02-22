c6c7ef778ee0ff9b0c2cbe4f24703ec4dd836851
sahil garg <gargs2483@gmail.com>
2022-02-22 13:18:47 +0530

    add bikes route

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const citiesController = require("./controllers/cities.controller");
 const locationsController = require("./controllers/locations.controller");
+const bikesController = require("./controllers/bikes.controller");
 
 const app = express();
 app.use(express.json());
 app.use("/api/cities", citiesController);
 app.use("/api/locations", locationsController);
+app.use("/api/bikes", bikesController);
 
 module.exports = app;
---
+++ src/controllers/bikes.controller.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const Bikes = require("../models/bike.model");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const bikes = await Bikes.find({}).lean().exec();
+
+    return res.status(200).send({ bikes });
+  } catch (err) {
+    res
+      .status(500)
+      .send({ status: "fail", message: err.message, err: { err } });
+  }
+});
+
+module.exports = router;
